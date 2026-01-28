9189ec6ee04bd091aecba2c836ab39335bcd9a42
MariaJoMarin <mariajomarin1312@gmail.com>
2026-01-28 13:27:02 -0600

    Se realizaron los cambios en la parte visiual del Login y  de el Menu Principal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace WalletSICAI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
